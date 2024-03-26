c2b4b7ddf65c2abccf6c08351c924d4bec1747f1
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-03-26 18:11:32 +0545

    PROJECT - GAME1

---
+++ P1-madlibs.py
@@ -0,0 +1,11 @@
+adjective = input ("enter an adjective")
+noun = input ("enter a noun")
+verb= input ("enter a verb")
+
+
+
+
+
+print(f"I ate a {adjective}")
+print(f"I saw a {noun}")
+print(f"The {noun} was {verb}ing")
