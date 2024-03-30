f7f86097bda00e9b0440fbe4e5bcebb9ac15cb51
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-03-30 17:03:47 +0545

    Create tryexpect.py

---
+++ tryexpect.py
@@ -0,0 +1,15 @@
+try:
+    b = int (input ("enter a number "))
+    a = 5/b  
+    print (a)
+
+
+      
+
+except ZeroDivisionError : 
+     print("NUMBER NOT DIVISIBLE BY ZERO")
+
+except ValueError :
+     print ("wrong input ")
+
+
