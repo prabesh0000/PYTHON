47493a7ca682f41a2fd3af37e23592782407cc5c
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-02 19:57:39 +0545

    last day of basic

---
+++ class2.py
@@ -0,0 +1,7 @@
+from sclass import student 
+
+student1 =  student ( " jim" , "sciecnce" , 3.1)
+
+print(student1.Name)
+print(student1.gpa)
+print(student1.major)
---
+++ sclass.py
@@ -0,0 +1,10 @@
+class student :
+    def __init__(self, Name , major , gpa) :  # this initialize function can map the class and contains the attributes of the class
+         self.Name =  Name
+         self.major = major
+         self.gpa = gpa 
+
+
+student2 = student ("prabesh " , "science" , 4 ) 
+
+print (student2.Name)  
