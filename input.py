ee5e716fdd5dc32b5cd3a21c6d6b2f6acfcf0e41
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-03 21:58:49 +0545

    Create input.py

---
+++ input.py
@@ -0,0 +1,9 @@
+n = int (input("enter the value"))
+
+print (n)
+
+
+# if we want to put our answer in the output we have to declare F  string in the begining 
+#example
+
+print (f"your value is {n}")
